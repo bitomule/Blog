af1f0f66853c5734fcccb5c40c43a56f80f0541b
David Collado <david.collado@wallapop.com>
2020-04-11 13:01:53 +0200

    Basic blog setup

---
+++ Content/posts/first-post.md
@@ -0,0 +1,8 @@
+---
+date: 2020-04-11 12:59
+description: A description of my first post.
+tags: first, article
+---
+# My first post
+
+My first post's text.
---
+++ Sources/Blog/main.swift
@@ -0,0 +1,25 @@
+import Foundation
+import Publish
+import Plot
+
+// This type acts as the configuration for your website.
+struct Blog: Website {
+    enum SectionID: String, WebsiteSectionID {
+        // Add the sections that you want your website to contain here:
+        case posts
+    }
+
+    struct ItemMetadata: WebsiteItemMetadata {
+        // Add any site-specific metadata that you want to use here.
+    }
+
+    // Update these properties to configure your website:
+    var url = URL(string: "https://blog.bitomule.com")!
+    var name = "Bitomule's learning shack"
+    var description = "My thoughts about iOS, technology or any other thing that comes to my mind."
+    var language: Language { .english }
+    var imagePath: Path? { nil }
+}
+
+// This will generate your website using the built-in Foundation theme:
+try Blog().publish(withTheme: .foundation)
